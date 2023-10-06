d223904c93e2874606ba38f3582231a4f8bf0e74
Vitor Sales <vitormalisa@gmail.com>
2023-10-05 23:08:24 -0300

    Requisito 3 feito

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-window.onload = () => {
-    pixelElemenstOfPaletteCreation();
-}
+// window.onload = () => {
+// }
 // Requisito 1: Criar titulo com id = 'title'. Conteudo: 'Paleta de Cores. Criar elemento para paleta de cores com id = 'color-palette' e cada cor individual dentro da paleta deve possuir a classe color.
 
 const bodyTag = document.getElementsByTagName('body')[0];
@@ -28,5 +27,49 @@ const pixelElemenstOfPaletteCreation = () => {
         colorPalette.appendChild(colorDiv);
     }
 };
+pixelElemenstOfPaletteCreation();
 
+const createElementWithIdFunction = (element, id, parent) => {
+    const thisElement = document.createElement(element);
+    thisElement.id = id;
+    parent.appendChild(thisElement);
+};
+const createElementWithClassFunction = (element, classe, parent) => {
+    const thisElement = document.createElement(element);
+    thisElement.className = classe;
+    parent.appendChild(thisElement);
+};
+// Requisito 2
+const boardOfPixelsCreation = () => {
+    createElementWithIdFunction('section', 'pixel-board', bodyTag);
+    const pixelBoard = document.getElementById('pixel-board');
+    pixelBoard.style.width = '210px';
+    pixelBoard.style.height = '210px';
+    for (let index = 0; index < 25; index += 1) {
+        createElementWithClassFunction('div', 'pixel', pixelBoard);
+        let pixel = document.querySelectorAll('.pixel')[index];
+        pixel.style.width = '40px';
+        pixel.style.height = '40px';
+        pixel.style.border = '1px solid black';
+        pixel.style.display = 'inline-block';
+        pixel.style.backgroundColor = 'white';
+    };
+};
+boardOfPixelsCreation();
 
+// Requisito 3: Crie função para selecionar a cor da paleta: uma cor por vez
+
+const divColor1 = document.querySelectorAll('.color')[0];
+divColor1.classList.add('selected');
+
+const colorSelection = (event) => {
+        let selected = document.querySelector('.selected');
+        selected.classList.remove('selected');
+        event.target.classList.add ('selected');
+};
+
+for (let index = 0; index < 4; index += 1) {
+    let divColor = document.querySelectorAll('.color')[index];
+    divColor.addEventListener('click', colorSelection);
+    
+}
